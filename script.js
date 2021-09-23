8ff121d254e7c9bef0c9842ef74a2287bd8c8065
John Rodrigues <john.rodrigues008@gmail.com>
2021-09-23 09:07:28 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Função simplificada do Botão Mobile */
+function onClickMenu(){
+    document.getElementById('navbar').classList.toggle("charge");
+    document.getElementById('nav').classList.toggle("charge");
+    document.getElementById('menu-bg').classList.toggle("charge");
+}
+/* Função simplificada Para envio da Mensagem no form (Sem aplicação com banco de dados / mensagem ao usuario) */
+function refresh(){
+    alert("Mensagem enviada com Sucesso!");
+}
